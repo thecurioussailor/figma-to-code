f4ea1fe7f0def08832a7924d9a85142b33084865
Ashutosh Sagar <48312681+thecurioussailor@users.noreply.github.com>
2024-07-21 15:58:58 +0530

    navbar list

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -27,7 +27,7 @@ function Navbar() {
         </div>
 
         {/* mobile plus tablet view */}
-            <div className='flex flex-col justify-between h-[80px] w-full top-0 fixed z-20 lg:hidden items-center bg-[#00000033] backdrop-blur-xl overflow-hidden'>
+            <div className='flex flex-col justify-between h-[80px] w-full top-0 fixed z-20 lg:hidden items-center bg-[#00000033] backdrop-blur-xl'>
                 <div className='flex justify-between items-center flex-row w-full'>
                     <div className='w-[87px] ml-[15px] md:ml-[56px] rounded-[30px]'>
                         <img src='https://s3-alpha-sig.figma.com/img/64fd/2ad3/fe72bd43def70cb321dc93b14a3b55fe?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=hjAazcXUgIjozcC0IbjfEuF9Ug4TOSYxpeHM8pcaylZIN9p2sPgtUYjZJ9g38m6Ko5gd-GwK1B0knkh-vB6pa2K~cxP4lFTqJyQjS5DJ1TBn2XLGRHXY4gJtxpXVurrQU6zm~f2StLEN0ir~Cp2lgwn5-zXUBzQfKq6R1h-ysKplZAs1HkwN4e~4FS~2xHbUactwt6XYfoxMcqktRLaKfF9dKjjuwkfc4Pb~TtbvzoH-g1V7LjvqTGf0Fv5Kklc6IsWS6COdFMzO2p9YEg8Q8KMm36~7vY32RJtp9fRmqvfWM2fBpv20EH0Mt2ZpjWC41ZfrELQMjli9~PD7l~5K6Q__'/>
